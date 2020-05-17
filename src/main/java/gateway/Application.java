cceb8fb26232a27b54be072f5af628fea8a3eb5d
raycu03 <raynarvaez1998@gmail.com>
2020-05-17 00:57:50 -0500

    Initial commit

---
+++ src/main/java/gateway/Application.java
@@ -0,0 +1,24 @@
+package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/refrescado")
+						.uri("http://localhost:8082"))
+				.build();
+	}
+}
